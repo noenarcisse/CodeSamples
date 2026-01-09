8670d183049f3abc584ba602890d896952ff4187
NoeN <narcisse.noe@gmail.com>
2026-01-09 16:05:13 +0100

    Create PrefabPooler.cs
    example of unity

---
+++ C#/UnityHelpers/PrefabPooler.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PrefabPooler : MonoBehaviour
+{
+    [Header("Instance refs and attributes")]
+    [Tooltip("Activer l'element a l'instanciation")]
+    public bool activated;
+    [Tooltip("Reference du parent auquel on accroche l'obj")]
+    public Transform parent;
+
+    [Tooltip("Liste des objets instanciable random")]
+    public List<GameObject> pfbAPool;
+    [Tooltip("Nombre a pooler")]
+    public int nPool;
+
+    public List<GameObject> pfbList;
+
+
+    void Start()
+    {
+        pfbList = new List<GameObject>();
+        for (int i = 0; i < nPool; i++)
+        {
+            GameObject obj =  Instantiate(pfbAPool[Random.Range(0, pfbAPool.Count)]);
+            obj.transform.SetParent(parent, false);
+            obj.SetActive(activated);
+
+            pfbList.Add(obj);
+        }
+    }
+
+}
